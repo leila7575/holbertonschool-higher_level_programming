34fc56bc36546b1efb8e8b28389527b1f7cff2d3
leila7575 <leila.louajri@gmail.com>
2024-01-23 14:25:03 +0100

    adding 1-last_digit file

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,18 @@
 import random
 number = random.randint(-10000, 10000)
 lastdigit = int(repr(number)[-1])
+if number < 0:
+    result = lastdigit * -1
+else:
+    result = lastdigit
+
 str1 = "Last digit of "
 str2 = " is "
 str3 = " and is greater than 5"
 str4 = " and is 0"
 str5 = " and is less than 6 and not 0"
 
-print(f"{str1} {number} {str2} {lastdigit}",end = " ")
+print(f"{str1} {number} {str2} {result}",end = " ")
 if lastdigit > 5:
     print(f"{str3}")
 elif lastdigit == 0:
