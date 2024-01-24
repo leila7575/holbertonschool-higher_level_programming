ba7a09c21665fe11467604fbaaa9745f9af5da5a
leila7575 <leila.louajri@gmail.com>
2024-01-24 17:32:02 +0100

    adding 6-print_comb3 file

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,5 @@ for i in range(10):
         if i * 10 + j == 89:
             print("{:02d}".format(i * 10 + j), end="")
         else:
-            print("{:02d}".format(i * 10 + j), end=", ")
+            print("{:02d}, ".format(i * 10 + j), end="")
+print()
