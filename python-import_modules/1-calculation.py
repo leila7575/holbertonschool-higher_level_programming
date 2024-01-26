51853a3567bf9f2873523b95cc6f1a1faf2ea118
leila7575 <leila.louajri@gmail.com>
2024-01-26 09:50:55 +0100

    adding 1-calculation.py file

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    addition = add(a, b)
-    substraction = sub(a, b)
-    multiplication = mul(a, b)
-    division = div(a, b)
-    print(f"{a} + {b} = {addition}")
-    print(f"{a} - {b} = {substraction}")
-    print(f"{a} * {b} = {multiplication}")
-    print(f"{a} / {b} = {division}")
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
