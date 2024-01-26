45bd9354887c71821b2c05a8ef66ec735661286c
leila7575 <leila.louajri@gmail.com>
2024-01-26 20:54:50 +0100

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    number_arg = len(sys.argv) - 1
+    args_list = sys.argv[1:]
+
+    if number_arg == 0:
+        print(f"{number_arg} arguments.")
+    elif number_arg == 1:
+        print(f"{number_arg} argument:")
+    else:
+        print(f"{number_arg} arguments:")
+    
+    if number_arg:
+        for i, arg in enumerate(args_list, 1):
+            print(f"{i}: {arg}")
