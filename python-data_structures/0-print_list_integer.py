12b7db494b59732f8b7f7386efc34191e3587372
leila7575 <leila.louajri@gmail.com>
2024-01-30 11:56:40 +0100

    adding 0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    list = my_list[0:5]
-    for i in list:
-        print("{}".format(i))
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
