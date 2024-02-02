8dd525d2f9cb7f8b1e49d257372f3e8db26e10c2
leila7575 <leila.louajri@gmail.com>
2024-02-02 23:07:41 +0100

    exec file O

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
-    try:
-        while i < x:
-            print("{:d}".format(my_list[i]), end="")
-            i += 1
-    except:
-        pass
+    counter = 0
+    for i in range(int(x)):
+        try:
+            print("{}".format(my_list[i]), end="")
+            counter += 1
+        except IndexError:
+            pass
     print()
-    return i
+    return counter
