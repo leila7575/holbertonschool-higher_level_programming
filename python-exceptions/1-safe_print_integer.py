c7d1e20dd5e3e4a3b1584c10208e4a363fd58db8
leila7575 <leila.louajri@gmail.com>
2024-02-02 20:59:01 +0100

    1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        print()
+        print("{:d}".format(int(value)))
         return True
     except ValueError:
         return False
