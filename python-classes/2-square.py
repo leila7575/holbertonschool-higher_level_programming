55ad334382b291ef2ee74bff4a8d1471988756ef
leila7575 <leila.louajri@gmail.com>
2024-02-08 11:03:50 +0100

    adding 2-square file

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square():
+    """This class defines a square, with size attribute."""
+
+    def __init__(self, size=0):
+        """Initialisation of the attributes of the class, initializes square with size.
+
+        Args:
+            size(int): private instance attribute, size of the square.
+        Raises:
+            TypeError: size must be an integer.
+            ValueError: if size is less than 0.
+
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
