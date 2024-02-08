faf64f9d68d33eea46b8155c3e916e858fb88ea5
leila7575 <leila.louajri@gmail.com>
2024-02-08 11:03:20 +0100

    adding 1-square file

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square():
+    """This class defines a square."""
+
+    def __init__(self,__size):
+        """Initialisation of the attributes of the class, initializes size attribute.
+
+        Args:
+            size(int): private instance attribute, size of the square
+        """
+        self.__size = __size
+
