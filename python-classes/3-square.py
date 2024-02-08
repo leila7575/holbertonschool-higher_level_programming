b22d2e9f18a32791e28e1ddefbbdb4cea0f31084
leila7575 <leila.louajri@gmail.com>
2024-02-08 11:04:15 +0100

    adding 3-square file

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+class Square():
+    """This class defines a square.
+    """
+    def __init__(self, size=0):
+        """Initialisation of the attributes of the class.
+
+        Args:
+            size(int): private instance attribute, size of square
+        Raises:
+            TypeError: size must be an integer.
+            ValueError: if size is less than 0.
+
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
+    def area(self):
+        """Returns square area based on size.
+
+        Returns:
+            square area(int)
+        """
+        return int(self.__size) ** 2
