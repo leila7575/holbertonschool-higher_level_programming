abee568ef3a5990e14e62c9616b1b13e9b4c1fca
leila7575 <leila.louajri@gmail.com>
2024-02-08 12:01:04 +0100

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,11 +7,12 @@ This module contains the class Square.
 class Square():
     """This class defines a square."""
 
-    def __init__(self,__size):
-        """Initialisation of the attributes of the class, initializes size attribute.
+    def __init__(self, __size):
+        """
+        Initialisation of the attributes of the class
+        initializes size attribute.
 
         Args:
             size(int): private instance attribute, size of the square
         """
         self.__size = __size
-
