1f9a690990734638ec49c074deee2a55c1b55d83
leila7575 <leila.louajri@gmail.com>
2024-02-09 22:53:23 +0100

    3-say_my_name.py

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains the function say_my_name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints My name is <first name> <last name>
+        Args:
+            first_name(str): first name
+            lest_name(str): last name
+        Raises:
+            TypeError: first_name must be a string
+            TypeError: last_name must be a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
