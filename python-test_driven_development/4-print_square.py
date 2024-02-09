eb718a03b7eb46d26f4cf241f72c868c9bc252cb
leila7575 <leila.louajri@gmail.com>
2024-02-09 23:30:59 +0100

    4-print_square.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains the function print_square.
+"""
+
+
+def print_square(size):
+    """Prints square with the character #
+        Args:
+            size(int): size of the square
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
