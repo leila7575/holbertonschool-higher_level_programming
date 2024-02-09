43808b039e27969b328dcb38f0a016e719cc62de
leila7575 <leila.louajri@gmail.com>
2024-02-09 17:29:21 +0100

    7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -102,10 +102,8 @@ class Rectangle():
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        result = ""
-        for i in range(self.__height):
-            result += str(self.print_symbol) * self.__width + "\n"
-        return result
+        
+        return ((str(self.print_symbol) * self.__width + "\n") * self.__height)[:-1]
 
     def __repr__(self):
         """Returns string representation of the rectangle object.
