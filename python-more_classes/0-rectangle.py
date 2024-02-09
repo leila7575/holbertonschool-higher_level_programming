4855def5684716cd885cc1ae49df81579aae128e
leila7575 <leila.louajri@gmail.com>
2024-02-09 14:16:13 +0100

    0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+This module contains the class Square.
+"""
+
+
+
 class Rectangle:
+     """This class defines a square.
+
+    Attributes:
+        None
+
+    """
     pass
