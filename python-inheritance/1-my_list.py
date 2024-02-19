169ae0ced23edf1f494b255d227f2743fc0c2687
leila7575 <leila.louajri@gmail.com>
2024-02-19 03:40:12 +0100

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,7 +5,9 @@ This module contains the class MyList.
 
 
 class MyList(list):
-    """class MyList that inherits from list with public istance method print_sorted"""
+    """class MyList that inherits from list
+    public istance method: print_sorted
+    """
     def print_sorted(self):
         """Sorts the list in ascending order."""
         print(sorted(self))
