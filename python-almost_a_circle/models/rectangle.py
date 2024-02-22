0a6996322695e1e95f1bab2e3c887ac0fb7a1187
leila7575 <leila.louajri@gmail.com>
2024-02-22 06:13:52 +0100

    adding rectangle file

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -30,7 +30,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """sets width of rectangle."""
-        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -40,7 +45,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """sets height of rectangle."""
-        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
