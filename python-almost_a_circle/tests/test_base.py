6c73e7bb03fa1a022191db9ade388079fdc934d6
leila7575 <leila.louajri@gmail.com>
2024-02-23 22:19:46 +0100

    adding test for assigning automaticaly ID

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -11,8 +11,10 @@ class TestBase(unittest.TestCase):
     """testing instantiation of Base class."""
     def setUp(self):
         """Runs before each test method."""
-        self.rect = Rectangle(1, 2, 3, 4, 5)
-        self.square = Square(6, 7, 8, 9)
+        self.rect1 = Rectangle(1, 2, 3, 4)
+        self.rect1 = Rectangle(5, 6, 7, 8)
+        self.square1 = Square(6, 7, 8)
+        self.square1 = Square(9, 10, 11)
 
     def test_to_json_string(self):
         self.assertEqual(Rectangle.to_json_string(None), "[]")
@@ -23,24 +25,29 @@ class TestBase(unittest.TestCase):
         self.assertEqual(Rectangle.from_json_string("[]"), [])
 
     def test_save_and_load_from_file(self):
-        Rectangle.save_to_file([self.rect])
-        Square.save_to_file([self.square])
+        Rectangle.save_to_file([self.rect1, self.rect2])
+        Square.save_to_file([self.square1, self.square2])
 
         rectangles = Rectangle.load_from_file()
         squares = Square.load_from_file()
 
-        self.assertIsInstance(rectangles[0], Rectangle)
-        self.assertIsInstance(squares[0], Square)
-        self.assertEqual(rectangles[0].id, 5)
-        self.assertEqual(rectangles[0].width, 1)
-        self.assertEqual(rectangles[0].height, 2)
-        self.assertEqual(rectangles[0].x, 3)
-        self.assertEqual(rectangles[0].y, 4)
+        self.assertEqual(rectangles[0].id, 1)
+        self.assertEqual(rectangles[1].id, 2)
 
-        self.assertEqual(squares[0].id, 9)
-        self.assertEqual(squares[0].size, 6)
-        self.assertEqual(squares[0].x, 7)
-        self.assertEqual(squares[0].y, 8)
+        self.assertEqual(squares[0].id, 3)
+        self.assertEqual(squares[1].id, 4)
+
+        new_rect = Rectangle(3, 4, 5, 6)
+        new_square = Square(7, 8, 9)
+
+        Rectangle.save_to_file([new_rect])
+        Square.save_to_file([new_square])
+
+        new_rectangles = Rectangle.load_from_file()
+        new_squares = Square.load_from_file()
+
+        self.assertEqual(new_rectangles[0].id, 3)
+        self.assertEqual(new_squares[0].id, 5)
 
     def tearDown(self):
         """Runs after each test method."""
@@ -53,4 +60,3 @@ class TestBase(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-#!/usr/bin/python3
