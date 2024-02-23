d0297c81cf10315e248b796c9e4dad2b585a3bbb
leila7575 <leila.louajri@gmail.com>
2024-02-23 20:25:07 +0100

    adding base file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module contains the class Base."""
+from json import dumps, loads
 
 
 class Base():
@@ -19,3 +20,57 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries.
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """"converts json string to a list of dictionaries."""
+        if json_string is None or not json_string:
+            return []
+        return loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes json string representation of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
+        list_objs = [obj.to_dictionary() for obj in list_objs]
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(cls.to_json_string(list_objs))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns instance with all attributes"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        if cls is Rectangle:
+            dummy_instance = Rectangle(1, 1)
+        elif cls is Square:
+            dummy_instance = Square(1)
+        else:
+            return None
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Return list of instances."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_data = file.read()
+        except FileNotFoundError:
+            return []
+
+        list_dicts = cls.from_json_string(json_data)
+        return [cls.create(**dict_obj) for dict_obj in list_dicts]
