12a32032fa04d6ef815f4ce6efc51f660145e123
leila7575 <leila.louajri@gmail.com>
2024-03-19 23:08:24 +0100

    adding 16-no_link file

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table of the database hbtn_0c_0
+-- except rows without name value, scores by descending order
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
