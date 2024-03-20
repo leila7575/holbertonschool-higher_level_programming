46d2f7994ee1d7e964462e1bac304546ef0ccc86
leila7575 <leila.louajri@gmail.com>
2024-03-20 22:56:34 +0100

    11-genre_id_all_shows.sql

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in hbtn_0d_tvshows, if show doesn't have genre NULL
+-- in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
