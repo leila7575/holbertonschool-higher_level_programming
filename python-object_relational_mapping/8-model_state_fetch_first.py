9843bdd2489c985e23b5148eddce3396cca5e8e8
leila7575 <leila.louajri@gmail.com>
2024-03-22 22:17:26 +0100

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,12 +8,19 @@ from model_state import Base, State
 
 if __name__ == "__main__":
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}')
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    engine = create_engine(f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}')
+
+
+    Base.metadata.create_all(engine)
+
+
     Session = sessionmaker(bind=engine)
     session = Session()
+
+
     state = session.query(State).order_by(State.id).first()
     if state is not None:
         print(f"{state.id}: {state.name}")
