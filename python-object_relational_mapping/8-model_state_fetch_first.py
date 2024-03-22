a2e89a52c7fc7470da994289ea6b2d7b2c729e8d
leila7575 <leila.louajri@gmail.com>
2024-03-22 22:03:33 +0100

    8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """lists all state objects from database."""
-import sys
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
 if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
     engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}')
     Session = sessionmaker(bind=engine)
     session = Session()
